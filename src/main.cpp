d49b579ea8299f9098a6ec37c08d642e3bdccd00
AIn0n <55995692+AIn0n@users.noreply.github.com>
2022-10-11 09:45:39 +0200

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <CL/opencl.hpp>
 #include <iostream>
 #include <fstream>
+#include <cassert>
 
 int main() 
 {
@@ -9,10 +10,8 @@ int main()
     cl_platform_id platforms[64];
     uint platformCount;
     cl_int res = clGetPlatformIDs(64, platforms, &platformCount);
-    if (res != CL_SUCCESS) {
-        std::cout << "error: " << res << " LINE: " << __LINE__ << '\n';
-        return 1;
-    }
+    assert(res == CL_SUCCESS);
+
     /* search thru devices and found any kind of GPU */
     cl_device_id device = nullptr;
     for (int i = 0; i < platformCount && device == nullptr; ++i) {
@@ -25,17 +24,11 @@ int main()
 
     /* context initalization */
     cl_context context = clCreateContext(nullptr, 1, &device, nullptr, nullptr, &res);
-    if (res != CL_SUCCESS) {
-        std::cout << "error: " << res << " LINE: " << __LINE__ << '\n';
-        return 1;
-    }
+    assert(res == CL_SUCCESS);
 
     /* command queue initialization */
     cl_command_queue queue = clCreateCommandQueue(context, device, 0, &res);
-    if (res != CL_SUCCESS) {
-        std::cout << "error: " << res << " LINE: " << __LINE__ << '\n';
-        return 1;
-    }
+    assert(res == CL_SUCCESS);
 
     std::string src = "\
     __kernel void vector_sum(__constant float* a, __constant float* b, __global float* c) {   \
@@ -51,10 +44,7 @@ int main()
 
     /* program initialization */
     cl_program program = clCreateProgramWithSource(context, 1, &source, &length, &res);
-    if (res != CL_SUCCESS) {
-        std::cout << "error: " << res << " LINE: " << __LINE__ << '\n';
-        return 1;
-    }
+    assert(res == CL_SUCCESS);
 
     /* check program build info */
     res = clBuildProgram(program, 1, &device, "", nullptr, nullptr);    
@@ -67,73 +57,43 @@ int main()
 
     /* kernel initalization */
     cl_kernel kernel = clCreateKernel(program, "vector_sum", &res);
-    if (res != CL_SUCCESS) {
-        std::cout << "error: " << res << " LINE: " << __LINE__ << '\n';
-        return 1;
-    }
+    assert(res == CL_SUCCESS);
 
     /* input buffer A prepare */
     cl_mem veca = clCreateBuffer(context, CL_MEM_READ_ONLY, 2 * sizeof(float), nullptr, &res);
-        if (res != CL_SUCCESS) {
-        std::cout << "error: " << res << " LINE: " << __LINE__ << '\n';
-        return 1;
-    }
+        assert(res == CL_SUCCESS);
 
     /* input buffer A fill */
-    float vecaData[] {15.0f, 4.0f};
+    float vecaData[] {15.0f, 8.0f};
     clEnqueueWriteBuffer(queue, veca, CL_TRUE, 0, 2 * sizeof(float), vecaData, 0, nullptr, nullptr);
-    if (res != CL_SUCCESS) {
-        std::cout << "error: " << res << " LINE: " << __LINE__ << '\n';
-        return 1;
-    }
+    assert(res == CL_SUCCESS);
 
     /* input buffer B prepare */
     cl_mem vecb = clCreateBuffer(context, CL_MEM_READ_ONLY, 2 * sizeof(float), nullptr, &res);
-        if (res != CL_SUCCESS) {
-        std::cout << "error: " << res << " LINE: " << __LINE__ << '\n';
-        return 1;
-    }
+    assert(res == CL_SUCCESS);
 
     /* input buffer B fill */
-    float vecbData[] {5.0f, 6.0f};
+    float vecbData[] {15.0f, 6.0f};
     clEnqueueWriteBuffer(queue, vecb, CL_TRUE, 0, 2 * sizeof(float), vecbData, 0, nullptr, nullptr);
-    if (res != CL_SUCCESS) {
-        std::cout << "error: " << res << " LINE: " << __LINE__ << '\n';
-        return 1;
-    }
+    assert(res == CL_SUCCESS);
 
     /* output buffer C prepare */
     cl_mem vecc = clCreateBuffer(context, CL_MEM_READ_ONLY, 2 * sizeof(float), nullptr, &res);
-    if (res != CL_SUCCESS) {
-        std::cout << "error: " << res << " LINE: " << __LINE__ << '\n';
-        return 1;
-    }
+    assert(res == CL_SUCCESS);
 
     /* add all buffers to kernel */
     res = clSetKernelArg(kernel, 0, sizeof(cl_mem), &veca);
-    if (res != CL_SUCCESS) {
-        std::cout << "error: " << res << " LINE: " << __LINE__ << '\n';
-        return 1;
-    }
+    assert(res == CL_SUCCESS);
     res = clSetKernelArg(kernel, 1, sizeof(cl_mem), &vecb);
-    if (res != CL_SUCCESS) {
-        std::cout << "error: " << res << " LINE: " << __LINE__ << '\n';
-        return 1;
-    }
+    assert(res == CL_SUCCESS);
     res = clSetKernelArg(kernel, 2, sizeof(cl_mem), &vecc);
-    if (res != CL_SUCCESS) {
-        std::cout << "error: " << res << " LINE: " << __LINE__ << '\n';
-        return 1;
-    }
+    assert(res == CL_SUCCESS);
 
     /* dimension size specification */
     size_t globalWorkSize = 2;
     size_t localWorkSize = 2;
     res = clEnqueueNDRangeKernel(queue, kernel, 1, 0, &globalWorkSize,  &localWorkSize, 0, nullptr, nullptr);
-    if (res != CL_SUCCESS) {
-        std::cout << "error: " << res << " LINE: " << __LINE__ << '\n';
-        return 1;
-    }
+    assert(res == CL_SUCCESS);
 
     /* read output buffer */
     float veccData[2] {0};
